38000ccf6fdde88e1b6dc03b5261e5b06b66582f
Burt Beckwith <burt@burtbeckwith.com>
2013-02-12 23:07:35 -0500

    I prefer to keep simple artifact implementations in one class file - feel free to revert

--- src/java/com/developerb/healtcontrol/artefact/HealthControlArtefactHandler.java
+++ src/java/com/developerb/healtcontrol/artefact/HealthControlArtefactHandler.java
@@ -1,6 +1,8 @@
 package com.developerb.healtcontrol.artefact;
 
+import org.codehaus.groovy.grails.commons.AbstractInjectableGrailsClass;
 import org.codehaus.groovy.grails.commons.ArtefactHandlerAdapter;
+import org.codehaus.groovy.grails.commons.InjectableGrailsClass;
 
 /**
  * @author Kim A. Betti <kim@developer-b.com>
@@ -13,8 +15,16 @@ public final class HealthControlArtefactHandler extends ArtefactHandlerAdapter {
         super(TYPE, HealthControlArtefactHandler.class, DefaultHealthControlGrailsClass.class, null);
     }
 
-    public boolean isArtefactClass(Class clazz) {
+    public boolean isArtefactClass(@SuppressWarnings("rawtypes") Class clazz) {
         return clazz != null && clazz.getName().endsWith(TYPE);
     }
 
+    public static interface HealthControlGrailsClass extends InjectableGrailsClass {
+    }
+
+    public static final class DefaultHealthControlGrailsClass extends AbstractInjectableGrailsClass implements HealthControlGrailsClass {
+        public DefaultHealthControlGrailsClass(Class<?> wrappedClass) {
+            super(wrappedClass, HealthControlArtefactHandler.TYPE);
+        }
+    }
 }
