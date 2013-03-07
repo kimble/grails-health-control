182a2b8ee6700c5265452cc31c83e8cf739add12
Kim A. betti <kim@developer-b.com>
2013-03-07 20:50:37 +0100

    Added tomcat to BuildConfig (not exported)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -21,7 +21,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build ":release:2.2.0", ":rest-client-builder:1.0.3", {
+        build (":release:2.2.0", ":rest-client-builder:1.0.3", ":tomcat:$grailsVersion") {
             export = false
         }
 
